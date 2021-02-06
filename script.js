9cadaf4c8d00cdd182b656a94b1a928f3fff1ea8
Roopesh Saravanan <helloiamroopesh@gmail.com>
2021-02-06 14:17:06 +0530

    advanced todo list
    advanced todo list second commit

--- script.js
+++ script.js
@@ -109,3 +109,69 @@ function updateSaveTodo(todos, todoId, Input) {
   loadTodos()
   location.reload()
 }
+
+// Date and Time
+
+const dayElement = document.querySelector('#day')
+const monthElement = document.querySelector('#month')
+const dateElement = document.querySelector('#date')
+const yearElement = document.querySelector('#year')
+let date = new Date()
+let day = date.getDay()
+let month = date.getUTCMonth()
+let newDate = date.getDate()
+
+function operationDay(day) {
+  if (day === 1) {
+    return 'Monday'
+  } else if (day === 2) {
+    return 'Tuesday'
+  } else if (day === 3) {
+    return 'Wednesday'
+  } else if (day === 4) {
+    return 'Thursday'
+  } else if (day === 5) {
+    return 'Friday'
+  } else if (day === 6) {
+    return 'Saturday'
+  } else if (day === 7) {
+    return 'Sunday'
+  } else {
+    return
+  }
+}
+
+let operationMonth = function (month) {
+  if (month === 0) {
+    return 'Jan'
+  } else if (month === 1) {
+    return 'Feb'
+  } else if (month === 2) {
+    return 'Mar'
+  } else if (month === 3) {
+    return 'Apr'
+  } else if (month === 4) {
+    return 'May'
+  } else if (month === 5) {
+    return 'Jun'
+  } else if (month === 6) {
+    return 'Jul'
+  } else if (month === 7) {
+    return 'Aug'
+  } else if (month === 8) {
+    return 'Sep'
+  } else if (month === 9) {
+    return 'Oct'
+  } else if (month === 10) {
+    return 'Nov'
+  } else if (month === 11) {
+    return 'Dec'
+  } else {
+    return
+  }
+}
+
+dayElement.innerHTML = operationDay(day)
+monthElement.innerHTML = operationMonth(month)
+dateElement.innerHTML = newDate
+yearElement.innerHTML = date.getFullYear()
