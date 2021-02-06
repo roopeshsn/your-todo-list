b3f0d5c658dd48010fb63dcf5433b7b5c0ea30cf
Roopesh Saravanan <helloiamroopesh@gmail.com>
2021-02-06 22:27:05 +0530

    advanced todo list updated
    advanced todo list updated and commited

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Selectors
-
+// Todo Selectors
 const form = document.querySelector('#new-todo-form')
 const todoInput = document.querySelector('#todo-input')
 const list = document.querySelector('#list')
@@ -8,11 +8,46 @@ const LOCAL_STORAGE_PREFIX = 'ADVANCED_TODO_LIST'
 const TODOS_STORAGE_KEY = `${LOCAL_STORAGE_PREFIX}-todos`
 const update = document.querySelector('.update')
 const saveButton = document.querySelector('#save-button')
+
+// Date and Time Selectors
+const dayElement = document.querySelector('#day')
+const monthElement = document.querySelector('#month')
+const dateElement = document.querySelector('#date')
+const yearElement = document.querySelector('#year')
+let date = new Date()
+let day = date.getDay()
+let month = date.getUTCMonth()
+let newDate = date.getDate()
+
+// Greetings Selectors
+let nameDiv = document.querySelector('#name-div')
+const USER_NAME = 'MAJOR'
+const TODOS_NAME_KEY = `${USER_NAME}-name`
+
+// Modal Popup Selectors
+const closeModalButtons = document.querySelectorAll('[data-close-button]')
+const overlay = document.getElementById('overlay')
+const modal = document.querySelector('#modal')
+const yesButton = document.querySelector('#yes-button')
+const noButton = document.querySelector('#no-button')
+const doneButton = document.querySelector('#done-button')
+
+// if condition to check whether the username is stored in local storage or not.
+if (operationName()) {
+  setTimeout(function () {
+    openModal()
+  }, 3000)
+}
+
+//To load the username
+loadName()
+
+//To load the todos
 let todos = loadTodos()
 todos.forEach(renderTodo)
 
 // Event Listeners
-
+// Todo Event Listeners
 list.addEventListener('change', (e) => {
   if (!e.target.matches('[data-list-item-checkbox]')) return
   const parent = e.target.closest('.list-item')
@@ -65,9 +100,7 @@ update.addEventListener('click', (e) => {
 })
 
 // Functions
-
 // Rendering Todos
-
 function renderTodo(todo) {
   const templateClone = template.content.cloneNode(true)
   const listItem = templateClone.querySelector('.list-item')
@@ -80,20 +113,17 @@ function renderTodo(todo) {
 }
 
 // Loading Todos from local storage
-
 function loadTodos() {
   const todosString = localStorage.getItem(TODOS_STORAGE_KEY)
   return JSON.parse(todosString) || []
 }
 
 // Saving Todos to local storage
-
 function saveTodos() {
   localStorage.setItem(TODOS_STORAGE_KEY, JSON.stringify(todos))
 }
 
 // Updating Todos to local Storage
-
 function updateTodo(todos, todoId) {
   const updateInput = document.querySelector('[data-update-input]')
   const todo = todos.find((t) => t.id == todoId)
@@ -110,17 +140,7 @@ function updateSaveTodo(todos, todoId, Input) {
   location.reload()
 }
 
-// Date and Time
-
-const dayElement = document.querySelector('#day')
-const monthElement = document.querySelector('#month')
-const dateElement = document.querySelector('#date')
-const yearElement = document.querySelector('#year')
-let date = new Date()
-let day = date.getDay()
-let month = date.getUTCMonth()
-let newDate = date.getDate()
-
+// Date and Time Functions
 function operationDay(day) {
   if (day === 1) {
     return 'Monday'
@@ -175,3 +195,68 @@ dayElement.innerHTML = operationDay(day)
 monthElement.innerHTML = operationMonth(month)
 dateElement.innerHTML = newDate
 yearElement.innerHTML = date.getFullYear()
+
+// Modal (popup) Event Listeners
+yesButton.addEventListener('click', (e) => {
+  nameDiv.classList.add('open')
+})
+
+noButton.addEventListener('click', (e) => {
+  autoName()
+  closeModal()
+  loadName()
+})
+
+doneButton.addEventListener('click', (e) => {
+  const input = document.querySelector('#name-input')
+  const usernameField = document.querySelector('#username')
+  const username = input.value
+  usernameField.innerHTML = username
+  saveName(username)
+  closeModal()
+})
+
+// Modal (popup) Functions
+closeModalButtons.forEach((button) => {
+  button.addEventListener('click', () => {
+    const modal = button.closest('.modal')
+    closeModal(modal)
+  })
+})
+
+function openModal() {
+  modal.classList.add('active') // if (modal == null) return
+  overlay.classList.add('active')
+}
+
+function closeModal() {
+  modal.classList.remove('active') //if (modal == null) return
+  overlay.classList.remove('active')
+}
+
+function saveName(name) {
+  localStorage.setItem(TODOS_NAME_KEY, JSON.stringify(name))
+  const username = document.querySelector('#username')
+  username.dataset.nameId = name
+}
+
+function loadName() {
+  const name = localStorage.getItem(TODOS_NAME_KEY)
+  const userField = document.querySelector('#username')
+  userField.innerHTML = JSON.parse(name)
+}
+
+function operationName() {
+  const name = localStorage.getItem(TODOS_NAME_KEY)
+  //console.log(name)
+  if (name == null) {
+    return true
+  } else {
+    return false
+  }
+}
+
+function autoName() {
+  const name = 'Major'
+  localStorage.setItem(TODOS_NAME_KEY, JSON.stringify(name))
+}
